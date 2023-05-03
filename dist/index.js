dc7ab9481a0a5e3c1236146d3123b4b4ac85ecc4
Rafal P | Raftech <rafal.pieniazek@raftech.nl>
2023-05-03 22:39:21 +0200

    adds dist build

--- dist/index.js
+++ dist/index.js
@@ -2841,14 +2841,14 @@ const wait = __nccwpck_require__(258);
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
+    const lockId = core.getInput('lock-id');
+    core.info(`Our lock-id is ${lockId}`);
 
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
+    // core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    // await wait(parseInt(ms));
+    // core.info((new Date()).toTimeString());
 
-    core.setOutput('time', new Date().toTimeString());
+    core.setOutput('exists', "false");
   } catch (error) {
     core.setFailed(error.message);
   }
